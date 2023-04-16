003768c72ab43250400fe7e4ba4c6522d21845ea
fab9410 <r.rayan200310@gmail.com>
2023-04-16 14:35:20 +0200

    Portfolio

---
+++ src/data/data.js
@@ -0,0 +1,35 @@
+import WorkIm from '../assets/project/workImg.jpeg'
+import RealEst from '../assets/project/realestate.jpg'
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:WorkIm,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:WorkIm,
+        github:"",
+        live:"",
+    },
+    {
+        id:4,
+        name:"React JS Application",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+
+
+]
